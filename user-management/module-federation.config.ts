d0eaf09ac43101f322682304ff685786f2acd926
Hoang Bui Son <hoang.buison@hitachivantara.com>
2024-01-01 19:06:11 +0700

    update config

--- user-management/module-federation.config.ts
+++ user-management/module-federation.config.ts
@@ -3,8 +3,8 @@ import { ModuleFederationConfig } from '@nx/webpack';
 const config: ModuleFederationConfig = {
   name: 'user-management',
   exposes: {
-    './Routes': 'apps/user-management/src/app/remote-entry/entry.routes.ts',
-    './Signin': 'apps/user-management/src/app/sign-in/sign-in.routes.ts',
+    './Routes': 'apps/user-management-module/user-management/src/app/remote-entry/entry.routes.ts',
+    './Signin': 'apps/user-management-module/user-management/src/app/sign-in/sign-in.routes.ts',
   },
 };
 
