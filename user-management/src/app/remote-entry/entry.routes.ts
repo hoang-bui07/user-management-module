76d1518fef2d6f4e3f1d59a27e4abaebc396eab1
Hoang Bui Son <hoang.buison@hitachivantara.com>
2024-01-09 09:59:01 +0700

    update code

--- user-management/src/app/remote-entry/entry.routes.ts
+++ user-management/src/app/remote-entry/entry.routes.ts
@@ -1,6 +1,13 @@
 import { Route } from '@angular/router';
 import { RemoteEntryComponent } from './entry.component';
+import { AuthGuard } from '@micro-app/auth';
 
 export const remoteRoutes: Route[] = [
-  { path: '', component: RemoteEntryComponent },
+  { 
+    path: '',
+    component: RemoteEntryComponent,
+    canActivate: [AuthGuard],
+    canLoad: [AuthGuard],
+    data: { roles: ['Admin'] }
+  },
 ];
